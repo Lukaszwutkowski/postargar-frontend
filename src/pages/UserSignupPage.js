9b4513adb69dc2c6e90bdcede7e372d754efbdb9
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2023-03-01 12:38:57 +0100

    Form to sign up method Done

---
+++ src/pages/UserSignupPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class UserSignupPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <div>
+          <input placeholder="Your display name"></input>
+        </div>
+        <div>
+          <input placeholder="Your username"></input>
+        </div>
+        <div>
+          <input placeholder="Your password" type="password"></input>
+        </div>
+        <div>
+          <input placeholder="Repeat your password" type="password"></input>
+        </div>
+        <div>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserSignupPage;
